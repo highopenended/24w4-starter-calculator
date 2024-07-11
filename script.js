b1efc1877963c8554ef9f1a817a6fa5bb1352373
Matt Glenn <highopenended@gmail.com>
2024-07-11 13:36:27 -0400

    Got number to string working

--- script.js
+++ script.js
@@ -3,10 +3,123 @@ let firstOperand = null
 let secondOperand = null
 let currentOperator = null
 
+let backgroundText=''
+
+const single_digit=['Zero','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+const double_underTwenty=['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+const double_overTwenty=['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+const higher_digits=['Thousand', 'Million', 'Billion', 'Trillion','Quadrillion','Quintillion','Sextillion','Septillion','Octillion','Nonillion','Decillion']
+
+
+
+function updateBackgroundText(text){
+  let myElement=document.querySelector(".backgroundText")
+  myElement.textContent=text
+
+}
+
+
+
+
 function updateDisplay() {
   const display = document.getElementById('display')
+  let displayStr=''
+  switch(displayValue){
+    case '8675309':
+      displayValue="Jenny"
+    default:
+  }
+  displayStr=getNumberWord(displayValue)
+  console.log(displayStr)
+  backgroundText=backgroundText+displayStr
+  updateBackgroundText(backgroundText)
   display.innerText = displayValue
 }
+
+
+function getNumberWord(num){
+  let arrTriples = getTriples(num)
+  let arrTripleWords = getTripleWords(arrTriples)
+  let hDigitWords=[]
+  let arrTripleWordsFinal =[]
+
+
+  for (let i = 0; i < arrTripleWords.length; i++) {
+    if(i>0){hDigitWords.push(higher_digits[i-1])}
+  }
+  hDigitWords.reverse()
+
+  for (let i = 0; i < arrTripleWords.length; i++) {
+    if(i<arrTripleWords.length-1){
+      arrTripleWordsFinal.push(`${arrTripleWords[i]} ${hDigitWords[i]}`)
+    }else{
+      arrTripleWordsFinal.push(`${arrTripleWords[i]}`)
+    }
+  }
+  // console.log(arrTripleWordsFinal.join(' '))
+  return arrTripleWordsFinal.join(' ')
+  
+
+
+  function getTripleWords(arr){
+    let returnArr=[]    
+    arr.map(triple=>{
+      const myNum=triple*1
+      switch (myNum.toString().length){
+        case 1: returnArr.push(getOneDigit(triple));break;
+        case 2: returnArr.push(getTwoDigit(triple));break;
+        case 3: returnArr.push(getThreeDigit(triple));break;
+      }
+    })
+    return returnArr
+
+
+    //  Returns a ONE digit number in text format
+    function getOneDigit(triple){return single_digit[triple[2]]}
+
+    //  Returns a TWO digit number in text format
+    function getTwoDigit(triple){
+      const myNum=triple*1
+      if(myNum<20){
+        return `${double_underTwenty[triple[2]]}`
+      } return `${double_overTwenty[triple[1]-2]} ${single_digit[triple[2]]}`
+    }
+
+    //  Returns a THREE digit number in text format
+    function getThreeDigit(triple){
+      const myNum=triple*1
+      let s1=`${single_digit[triple[0]]} Hundred `
+      let s2=getTwoDigit(triple)
+      return `${s1}${s2}`
+    }
+  }
+
+
+
+  // Groups of 3 single-digit numbers (58 would be [0,5,8])
+  function getTriples(num){
+    let returnArr=[]
+    let remainder=num.length%3
+    let numArr =[]
+    if(remainder!==0){
+      for (let i = 0; i < 3-remainder; i++) {
+        numArr.push('0')
+      }
+    }
+    for (let i = 0; i < num.length; i++) {
+      numArr.push(num.substring(i,i+1))
+    }
+    for (let i = 0; i < numArr.length/3; i++) {
+      const startIdx=i*3
+      const tArr=[numArr[startIdx],numArr[startIdx+1],numArr[startIdx+2]]
+      returnArr.push(tArr.join(''))
+    }
+    return returnArr
+  }
+}
+
+
+
 
 function appendNumber(number) {
   if (displayValue === '0') {
@@ -48,6 +161,7 @@ function clearDisplay() {
   firstOperand = null
   secondOperand = null
   currentOperator = null
+  backgroundText=''
   updateDisplay()
 }
 
