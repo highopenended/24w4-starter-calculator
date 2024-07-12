af0284c566fa5eedd543ef0b3fcb6c3776c6c0e6
Matt Glenn <highopenended@gmail.com>
2024-07-11 21:14:01 -0400

    Implemented flying numbers

--- script.js
+++ script.js
@@ -3,64 +3,77 @@ let firstOperand = null
 let secondOperand = null
 let currentOperator = null
 
-let backgroundText=''
-
-const single_digit=['Zero','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
-const double_underTwenty=['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
-const double_overTwenty=['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-const higher_digits=['Thousand', 'Million', 'Billion', 'Trillion','Quadrillion','Quintillion','Sextillion','Septillion','Octillion','Nonillion','Decillion']
+let prevEl;
 
 
-
-function updateBackgroundText(text){
-  let myElement=document.querySelector(".backgroundText")
-  myElement.textContent=text
-
-}
-
-
-
+// const single_digit=['Zero','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+// const double_underTwenty=['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+// const double_overTwenty=['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+// const higher_digits=['Thousand', 'Million', 'Billion', 'Trillion','Quadrillion','Quintillion','Sextillion','Septillion','Octillion','Nonillion','Decillion']
 
 function updateDisplay() {
   const display = document.getElementById('display')
-  let displayStr=''
+
   switch(displayValue){
     case '8675309':
       displayValue="Jenny"
     default:
   }
-  displayStr=getNumberWord(displayValue)
-  console.log(displayStr)
-  backgroundText=backgroundText+displayStr
-  updateBackgroundText(backgroundText)
+  addSlidingEl(displayValue.substring(displayValue.length-1,displayValue.length))
+  // addSlidingEl(displayValue)
+  console.log(getTriples(displayValue))
   display.innerText = displayValue
 }
 
+function addSlidingEl(num){
+  let newEl = document.createElement('p')
+  newEl.classList.add('backgroundText','animation_1')
+  newEl.textContent=num
 
-function getNumberWord(num){
+  let newTop = Math.floor(Math.random() * 50) + '%';
+  console.log(newTop)
+  newEl.style.top=newTop
+  if(prevEl){
+    removeEl(prevEl)
+  }
+  document.body.append(newEl)
+  prevEl=newEl
+}
+
+function removeEl(El){
+  setTimeout(()=>{
+    El.remove()
+    console.log('done')
+  },2000)
+}
+
+
+
+
+
+
+function getNumberWordArr(num){
   let arrTriples = getTriples(num)
   let arrTripleWords = getTripleWords(arrTriples)
   let hDigitWords=[]
   let arrTripleWordsFinal =[]
 
-
   for (let i = 0; i < arrTripleWords.length; i++) {
-    if(i>0){hDigitWords.push(higher_digits[i-1])}
+    if(i>0){
+      hDigitWords.push(higher_digits[i-1])
+    }
   }
   hDigitWords.reverse()
 
   for (let i = 0; i < arrTripleWords.length; i++) {
     if(i<arrTripleWords.length-1){
-      arrTripleWordsFinal.push(`${arrTripleWords[i]} ${hDigitWords[i]}`)
+      arrTripleWordsFinal.push(`${arrTripleWords[i]}-${hDigitWords[i]}`)
     }else{
       arrTripleWordsFinal.push(`${arrTripleWords[i]}`)
     }
   }
-  // console.log(arrTripleWordsFinal.join(' '))
-  return arrTripleWordsFinal.join(' ')
+  return arrTripleWordsFinal.join("\n")
   
-
-
   function getTripleWords(arr){
     let returnArr=[]    
     arr.map(triple=>{
@@ -71,54 +84,55 @@ function getNumberWord(num){
         case 3: returnArr.push(getThreeDigit(triple));break;
       }
     })
-    return returnArr
 
+    console.log('ReturnArr: ', returnArr)
+    return returnArr
 
     //  Returns a ONE digit number in text format
     function getOneDigit(triple){return single_digit[triple[2]]}
 
     //  Returns a TWO digit number in text format
     function getTwoDigit(triple){
-      const myNum=triple*1
-      if(myNum<20){
+      const dub=[triple[1],triple[2]].join('')*1
+      if(dub<10){
+        return `${single_digit[triple[2]]}`
+      }else if(dub<20){
         return `${double_underTwenty[triple[2]]}`
-      } return `${double_overTwenty[triple[1]-2]} ${single_digit[triple[2]]}`
+      }else{    
+        return `${double_overTwenty[triple[1]-2]}-${single_digit[triple[2]]}`
+      }
     }
 
     //  Returns a THREE digit number in text format
     function getThreeDigit(triple){
       const myNum=triple*1
-      let s1=`${single_digit[triple[0]]} Hundred `
+      let s1=`${single_digit[triple[0]]}-Hundred `
       let s2=getTwoDigit(triple)
-      return `${s1}${s2}`
+      return `${s1} and ${s2}`
     }
-  }
-
-
-
-  // Groups of 3 single-digit numbers (58 would be [0,5,8])
-  function getTriples(num){
-    let returnArr=[]
-    let remainder=num.length%3
-    let numArr =[]
-    if(remainder!==0){
-      for (let i = 0; i < 3-remainder; i++) {
-        numArr.push('0')
-      }
-    }
-    for (let i = 0; i < num.length; i++) {
-      numArr.push(num.substring(i,i+1))
-    }
-    for (let i = 0; i < numArr.length/3; i++) {
-      const startIdx=i*3
-      const tArr=[numArr[startIdx],numArr[startIdx+1],numArr[startIdx+2]]
-      returnArr.push(tArr.join(''))
-    }
-    return returnArr
   }
 }
 
-
+// Groups of 3 single-digit numbers (58 would be [0,5,8])
+function getTriples(num){
+  let returnArr=[]
+  let remainder=num.length%3
+  let numArr =[]
+  if(remainder!==0){
+    for (let i = 0; i < 3-remainder; i++) {
+      numArr.push('0')
+    }
+  }
+  for (let i = 0; i < num.length; i++) {
+    numArr.push(num.substring(i,i+1))
+  }
+  for (let i = 0; i < numArr.length/3; i++) {
+    const startIdx=i*3
+    const tArr=[numArr[startIdx],numArr[startIdx+1],numArr[startIdx+2]]
+    returnArr.push(tArr.join(''))
+  }
+  return returnArr
+}
 
 
 function appendNumber(number) {
@@ -137,6 +151,7 @@ function appendOperator(operator) {
   firstOperand = parseFloat(displayValue)
   currentOperator = operator
   displayValue = '0'
+  addSlidingEl(currentOperator)
 }
 
 function calculate() {
@@ -161,7 +176,6 @@ function clearDisplay() {
   firstOperand = null
   secondOperand = null
   currentOperator = null
-  backgroundText=''
   updateDisplay()
 }
 
